ca47c3555f2de1997527f6883d3d0b82ec7eb0ca
Ruben Reyes <ruben.r.reyes@gmail.com>
2017-05-14 14:12:41 -0700

    twitter sucks

--- liri.js
+++ liri.js
@@ -1,14 +1,18 @@
 var keys = require('./keys.js');
-
 var Twitter = require("Twitter");
+var spotify = require('spotify');
 
 var userInput = process.argv[2];
 
 if(userInput==="my-tweets"){
-    var params = {screen_name: 'RRutCodingBC'};
+    var client = new Twitter(keys);
+    var params = {screen_name: 'RRutCodingBC', count:20};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
-    console.log(tweets);
+        console.log(tweets);
+    }
+    if(error){
+        console.log(error);
     }
 });
 
